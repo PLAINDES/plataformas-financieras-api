f66b1d7508cc88dc036340e025ecd419dd61eaee
ChristianPE1 <cpardave@unsa.edu.pe>
2026-04-27 23:37:23 -0500

    ci: configure github actions workflow and docker compose profiles for automated testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,4 @@ alembic upgrade head
 
 echo "Seeding database with initial data..."
 python -m seeds.seed_from_sql
-
-echo "Starting Uvicorn server..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec "$@"
